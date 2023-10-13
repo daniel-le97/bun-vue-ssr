0fc9354214ade14b9b4caabc2a49be118520f543
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2023-10-13 14:48:27 -0600

    add dev server

--- dev.ts
+++ dev.ts
@@ -1,209 +1,114 @@
-const timer = Bun.nanoseconds()
-import { createConsola } from "consola";
-import { vue } from "./plugins/vue";
-import * as path from "path";
-import { existsSync, readdirSync, rmSync, statSync } from "fs";
-import type { ServeOptions } from "bun";
-import { renderToString } from "vue/server-renderer";
-import { createApp } from "./entry/index";
-import { FileSystemRouter } from "bun";
-// import { scssPlugin} from "./plugins/scss";
-const logger = createConsola();
+import { Subprocess, spawn } from "bun";
+import { WatchEventType, watch } from "fs";
+import Elysia from "elysia";
+import { Server } from 'bun'
+import { build } from "./build.ts";
+import { buildCache, buildServerCache } from "./plugins/utils/cache.ts";
+import consola from "consola";
 
+let serverProcess: Subprocess | null = null;
+let isRestarting = false;
 
-logger.info( 'registering server plugins' );
-// run your server plugins before any file/import processing
-// Bun.plugin( vue( {target: 'bun'} ) );
+const server: { instance: null | Server } = { instance: null }
 
-logger.success( 'server plugins registered' );
-
-
-// constants
-const isProd = process.env.NODE_ENV === 'production';
-const PROJECT_ROOT = import.meta.dir;
-const PUBLIC_DIR = path.resolve( PROJECT_ROOT, "public" );
-const BUILD_DIR = path.resolve( PROJECT_ROOT, ".build" );
-const ASSETS_DIR = path.resolve( PROJECT_ROOT, 'assets' );
-const serveDirectories = [ BUILD_DIR + '/client', ASSETS_DIR, PROJECT_ROOT, PUBLIC_DIR ];
-const port = process.env.PORT || 3000
-// const indexCSS = '/main.css';
-
-// get the pages from the filesystem to add as entrypoints for our bundler
-const srcRouter = new FileSystemRouter( {
-  dir: './pages',
-  style: "nextjs",
-  fileExtensions: [ '.vue' ]
-} );
-
-// we want to clear our build directory for the bundling that follows after this
-if ( existsSync( BUILD_DIR ) )
-  logger.info( 'clearning build dir ', BUILD_DIR );
-rmSync( BUILD_DIR, { recursive: true, force: true } );
-logger.success( 'build dir cleaned', BUILD_DIR );
-
-// console.log(srcRouter);
-
-
-// build our vue ssr app
-logger.start( 'bundling client' );
-const build = await Bun.build( {
-  entrypoints: [ import.meta.dir + '/entry/entry-client.ts', ...Object.values( srcRouter.routes )],
-  outdir: BUILD_DIR + '/client',
-  splitting: true,
-  target: 'browser',
-  plugins: [
-    vue( {target: 'browser'} ),
-    // scssPlugin,
-    // otherFiles,
-  ],
-  minify: false,
-  define: {
-    __VUE_OPTIONS_API__: "true",
-    __VUE_PROD_DEVTOOLS__: "true"
-  }
-} );
-
-if (!build.success) {
-  logger.error(build)
-  process.exit(1)
-}
-
-// this is the router for built pages
-const buildRouter = new Bun.FileSystemRouter( {
-  dir: BUILD_DIR + '/client/pages',
-  style: "nextjs",
-} );
-
-
-// helper function to find all files in all directories - not currently used
-function getAllFiles ( directories: string[] ): string[] {
-  let files: string[] = [];
-
-  function traverseDirectory ( dir: string ) {
-    const entries = readdirSync( dir, { withFileTypes: true } );
-
-    for ( const entry of entries )
-    {
-      const fullPath = path.join( dir, entry.name );
-
-      if ( entry.isDirectory() )
-      {
-        traverseDirectory( fullPath );
-      } else
-      {
-        files.push( fullPath );
-      }
-    }
-  }
-
-  for ( const directory of directories )
-  {
-    traverseDirectory( directory );
-  }
-
-  return files;
-}
-// helper function to serve files from the directory - .build/assets/public
-function serveFromDir (
-  serveDirectories: string[],
-  reqPath: string
-): Response {
-  for ( const dir of serveDirectories )
-  {
-    try
-    {
-      let pathWithSuffix = path.join( dir, reqPath );
-      const stat = statSync( pathWithSuffix );
-
-      if ( stat && stat.isFile() )
-      {
-
-        return new Response( Bun.file( pathWithSuffix ) );
-      }
-      continue;
-    } catch ( error )
-    {
-      //do something here if the file should have been found from the directory
-    }
-  }
-  return null;
-}
-
-// helper function to update our html and send it
-async function serveFromRouter ( request: Request ) {
-  try
-  {
-    const match = srcRouter.match( request);
-    // console.log(match);
-    
-
-    if ( match )
-    {
-      const builtMatch = buildRouter.match( request );
-      // console.log(builtMatch);
-      
-      if ( !builtMatch )
-      {
-        return new Response( "builtMatch not found", { status: 500 } );
-      }
-
-      let html = await Bun.file( './index.html' ).text();
-
-      const page = await createApp( match.filePath );
-   
-      
-      const stream = await renderToString( page.app );
-
-                  // set the page javascript we want to fetch for client
-      html = html.replace( '{{ dynamicPath }}', '/pages/' + builtMatch.src )
-                  // add the server side html to the html markup
-                 .replace( '<!--html-body-->', stream )
-
-
-      // send the finalized html
-      return new Response( html, {
-        headers: { "Content-Type": "text/html;charset=utf-8" },
-      } );
-    }
-  } catch ( error )
-  {
-    // do something here if the request should have been processed
-  }
-
-}
-
-
-// basic Bun native server to serve our app
-export default {
-  port,
-  async fetch ( request ) {
-console.log(request.url);
-
-    const routerResponse = await serveFromRouter( request );
-    if ( routerResponse )
-    {
-      return routerResponse;
-    }
-    let reqPath = new URL( request.url ).pathname;
-    if ( reqPath === "/" )
-    {
-      reqPath = "/index.html";
-    }
-
-    const serveDirectory = serveFromDir( serveDirectories, reqPath );
-    if ( serveDirectory )
-    {
-      return serveDirectory;
-    }
-
-    return new Response( "File not found", {
-      status: 404,
-    } );
+// we need a seperate server instance because the main one will turn off while reloading
+const app = new Elysia()
+.ws('/ws', {
+  message(ws, message) {
+      ws.send('hello world!')
   },
-} satisfies ServeOptions;
+  open ( ws ) {
+    ws.subscribe('refreshEvent')
+    consola.info("LiveReload listening on ws://localhost:8127/ws");
+  }
+}).listen(8127, (serv) =>{
+    server.instance = serv
+})
 
-const end = Bun.nanoseconds()
-logger.box( `http://localhost:${port}` , '\nready in', (end - timer) / 1e9);
-// logger.log((end - timer) / 1e9);
+
+function startServer() {
+    isRestarting = true;
+    consola.start( 'Starting server...' );
+    serverProcess = spawn( {
+        cmd: [ 'bun', 'index.ts', '--hot' ],
+        env: Bun.env,
+        stdio: [ 'inherit', 'inherit', 'inherit' ]
+    } );
+    const exitCode = serverProcess.exitCode;
+    if ( exitCode )
+    {
+        
+        process.exit( exitCode );
+    }
+    return serverProcess;
+}
+
+// Start the server initially
+startServer();
 
 
+
+
+const fileWatch = async ( event: WatchEventType, filename: string | Error | undefined ) => {
+    try {
+        if ( filename instanceof Error )
+        {
+            process.exit( 1 );
+        }
+        
+        if ( !filename || event !== 'change' )
+        {
+            return;
+        }
+        
+        const exclude = [ 'build', 'node_modules', '.git' , '.css', 'README'];
+        if ( exclude.find( excl => filename?.includes( excl ) ) )
+        {
+            return;
+        }
+        if ( !serverProcess )
+        {
+            return;
+        }
+        
+        consola.info( `Detected ${ event } in ${ filename }` );
+        buildCache.forEach( ( value, key ) => {
+            // key.includes(filename)
+            if ( key.includes( filename ) )
+            {
+                buildCache.set( key, '' );
+            }
+        } );
+        buildServerCache.forEach( ( value, key ) => {
+            // key.includes(filename)
+            if ( key.includes( filename ) )
+            {
+                buildServerCache.set( key, '' );
+            }
+        } );
+
+        const start = performance.now()
+        await build( false )
+        const end = performance.now();
+        const elapsedMilliseconds = end - start
+        consola.info( `rebundled in: ${ elapsedMilliseconds } ms` );
+        
+        isRestarting = false;
+        // console.log('refreshed in ', (end - start) / 1000000);
+        
+        server.instance?.publish('refreshEvent', 'reload')
+    } catch (error) {
+        consola.warn(error)
+        serverProcess ? serverProcess.kill(1) : ''
+        process.exit(1)
+    }
+        
+    };
+    
+    
+    
+    const watcher = watch(
+    import.meta.dir,
+    { recursive: true },
+    fileWatch
+);
